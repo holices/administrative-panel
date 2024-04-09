f6f55bb08ba88c1108f1801979a87cf78406b691
holices <dev.holices@gmail.com>
2024-04-09 17:26:18 +0100

    added antialiased

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -13,21 +13,19 @@ import { Logo } from './Logo'
 import { NavItem } from './NavItem'
 import { Widget } from './Widget'
 import { Profile } from './Profile'
+import { InputControl, InputPrefix, InputRoot } from '../Input'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
 
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <div className="flex items-center gap-2">
+      <InputRoot>
+        <InputPrefix>
           <Search className="h-5 w-5 text-zinc-500" />
-          <input
-            className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none"
-            placeholder="Search"
-          />
-        </div>
-      </div>
+        </InputPrefix>
+        <InputControl placeholder="Search" />
+      </InputRoot>
 
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
