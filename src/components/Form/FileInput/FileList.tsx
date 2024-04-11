a46b2ed33e7d2dfbbc13fe47916830efa5b709e4
holices <dev.holices@gmail.com>
2024-04-11 15:18:32 +0100

    created file list

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { UploadCloud } from 'lucide-react'
+import { Trash2, UploadCloud } from 'lucide-react'
 import { useFileInput } from './Root'
 import { formatBytes } from '@/utils/format-bytes'
 
@@ -28,7 +28,21 @@ export function FileList() {
                   {formatBytes(file.size)}
                 </span>
               </div>
+
+              <div className="flex w-full items-center gap-3">
+                <div className="h-2 flex-1 rounded-full bg-zinc-100">
+                  <div className="h-2 w-4/5 rounded-full bg-violet-600" />
+                </div>
+                <span className="text-sm font-medium text-zinc-700">80%</span>
+              </div>
             </div>
+
+            <button
+              type="button"
+              className="ml-auto rounded-md p-2 hover:bg-zinc-50"
+            >
+              <Trash2 className="h-5 w-5 text-zinc-500" />
+            </button>
           </div>
         )
       })}
