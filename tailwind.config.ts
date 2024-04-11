af1ded3ee931c9e598a01d7a085b9d752be2dc42
holices <dev.holices@gmail.com>
2024-04-11 09:24:33 +0100

    added new color prop

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,25 @@ const config: Config = {
   theme: {
     extend: {
       gridTemplateColumns: {
-        app: 'minmax(18rem, 20rem) 1fr',
+        app: 'minmax(16rem, 18rem) 1fr',
         profile: 'max-content 1fr min-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
 
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+
+      borderWidth: {
+        6: '6px',
+      },
+
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
       },
     },
   },
