207b4b98d31368909c35607e49e2e140fdb65438
holices <dev.holices@gmail.com>
2024-04-11 16:52:03 +0100

    created Select component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { SettingsTabs } from '../components/SettingsTabs'
 
 import { Mail } from 'lucide-react'
 import { Select } from '@/components/Form/Select'
+import { SelectItem } from '@/components/Form/Select/SelectItem'
 
 export default function Home() {
   return (
@@ -116,7 +117,10 @@ export default function Home() {
             >
               Country
             </label>
-            <Select />
+            <Select placeholder="Select a country">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United States" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 border-b border-zinc-200 pb-5 pt-5">
@@ -126,7 +130,16 @@ export default function Home() {
             >
               Timezone
             </label>
-            <div></div>
+            <Select placeholder="Select a country">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (UTC-08:00)"
+              />
+              <SelectItem
+                value="utc3"
+                text="América - São Paulo (UTC -03:00)"
+              />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 border-b border-zinc-200 pb-5 pt-5">
