816c9770db5e62b40de88210e518b3887ff30980
holices <dev.holices@gmail.com>
2024-04-11 11:29:19 +0100

    add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -153,6 +153,7 @@ export default function Home() {
             </label>
             <FileInput.Root>
               <FileInput.Trigger />
+              <FileInput.FileList />
               <FileInput.Control multiple />
             </FileInput.Root>
           </div>
