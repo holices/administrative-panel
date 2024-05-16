8b9993ed0b7ec87b6117c961ab3ef24948abe473
Holices <dev.holices@gmail.com>
2024-05-16 22:47:45 +0100

    sidebar responsitivity

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -17,7 +17,7 @@ import * as Input from '@/components/Input'
 
 export function Sidebar() {
   return (
-    <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
+    <aside className="flex bottom-0 flex-col gap-6 border-b border-zinc-200 p-4 fixed left-0 top-0 right-0 z-20 bg-white lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:relative">
       <Logo />
 
       <Input.Root>
